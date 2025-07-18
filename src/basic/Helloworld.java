a2f528912191981570cd283b2b8313df546bb09d
Anjola O <anjola.jobi@gmail.com>
2025-07-18 19:33:16 +0100

    side

--- src/basic/Helloworld.java
+++ src/basic/Helloworld.java
@@ -3,7 +3,7 @@ package basic;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("THIS IS A COMBINED VERSION FROM MAIN AND TEST!");
+		System.out.println("SIDE: THIS IS THE SIDE!");
 	}
 
 }
