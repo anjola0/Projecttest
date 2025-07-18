cd90948fa1e24c90caca8580391db754dc4d5546
Anjola O <anjola.jobi@gmail.com>
2025-07-18 19:16:23 +0100

    update greeting message in main

--- src/basic/Helloworld.java
+++ src/basic/Helloworld.java
@@ -3,7 +3,7 @@ package basic;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from MAIN branch!");
+		System.out.println("MAIN says: Welcome to the lottery!");
 	}
 
 }
